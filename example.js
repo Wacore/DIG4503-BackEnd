a6bf073ec384bbdf04692d9ab9d933bb858db4ae
Hongda Zheng <derrickzheng00@gmail.com>
2021-02-15 14:12:25 -0500

    Lab5 inital commit

--- example.js
+++ example.js
@@ -1,13 +1,30 @@
-let p = new Promise((res, rej) => {
-  let x = 0;
-  if (x == 0) res("sucessful");
-  if (x != 0) rej("Something went wrong.");
-});
+// let p = new Promise((res, rej) => {
+//   let x = 0;
+//   if (x == 0) res("sucessful");
+//   if (x != 0) rej("Something went wrong.");
+// });
 
-p.then((value) => {
-  console.log(value);
-}).catch((err) => {
-  console.log("Error: " + err);
-});
+// p.then((value) => {
+//   console.log(value);
+// }).catch((err) => {
+//   console.log("Error: " + err);
+// });
 
-setTimeout(() => console.log("1, 2, 3, 4, 5..."), 5000);
+// setTimeout(() => console.log("1, 2, 3, 4, 5..."), 5000);
+
+function swap(a, b) {
+  let temp = a;
+  a = b;
+  b = temp;
+  console.log(`a: ${a}, b: ${b}`);
+}
+
+const swap = (a, b) => {
+  let temp = a;
+  a = b;
+  b = temp;
+  console.log(`a: ${a}, b: ${b}`);
+};
+
+let arr = ["Hello", "World"];
+let [greeting, name] = arr;
---
+++ lab5/src/components/Posts.js
@@ -0,0 +1,35 @@
+import React, { Component, useEffect, useState } from "react";
+import axios from "axios";
+
+const Posts = () => {
+  // using useState to define a state called Posts
+  const [Posts, setPosts] = useState();
+
+  // using useEffect to fetch data from the API.
+  useEffect(() => {
+    // using axios to call the API
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
+      // if fullfilled, update the contents of the posts
+      setPosts(res.data);
+    });
+  }, []);
+
+  // render the Posts data
+  return (
+    <div>
+      <h1>Posts</h1>
+      {Posts &&
+        Posts.map((p) => {
+          const { id, title, body } = p;
+          return (
+            <div key={id}>
+              <h3>Title: {title}</h3>
+              <p>{body}</p>
+            </div>
+          );
+        })}
+    </div>
+  );
+};
+
+export default Posts;
