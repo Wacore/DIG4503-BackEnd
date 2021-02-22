04676c787bd97555ca9d56321189a87a17c516c7
Hongda Zheng <derrickzheng00@gmail.com>
2021-02-22 10:13:25 -0500

    Adding comment

--- lab6/index.js
+++ lab6/index.js
@@ -1,14 +1,21 @@
+// Include express to the projec
 const express = require("express");
 const app = express();
 
+// declare port
 const port = 3000;
 
+// root route
 app.get("/", (req, res) => {
+  // return a message when a user get access to the route
   res.send("Hello World!");
 });
 
+// public route
 app.use("/public", express.static("public"));
 
+// Listen to port 3000
 app.listen(port, () => {
+  // call-back: when the server starts, this message will appear on the concole.
   console.log(`Listening on port ${port}...`);
 });
